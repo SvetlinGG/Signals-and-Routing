28a33b2085e7109ef8caa206f34f9322d6ecb222
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-28 23:17:15 +0100

    set signal on app

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'signals';
+
+  counter = signal(0);
+
+  ngOnInit(): void {
+    console.log(`counter: ${this.counter()}`);
+    
+  }
+
+  increment(){
+
+    const value = this.counter() + 1;
+    this.counter.set(value);
+    
+    
+    
+
+  }
 }
