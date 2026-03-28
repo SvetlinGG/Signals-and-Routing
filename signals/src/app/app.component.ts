fa58ad8c58f23f18d5ad028ddbd653024a2158d8
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-28 23:22:41 +0100

    cfreate wrapper component

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -1,30 +1,15 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WrapperComponent } from './wrapper/wrapper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WrapperComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit  {
-  title = 'signals';
+export class AppComponent {
 
-  counter = signal(0);
-
-  ngOnInit(): void {
-    console.log(`counter: ${this.counter()}`);
-    
-  }
-
-  increment(){
-
-    const value = this.counter() + 1;
-    this.counter.set(value);
-    
-    
-    
-
-  }
+  
 }
---
+++ signals/src/app/wrapper/wrapper.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-wrapper',
+  standalone: true,
+  imports: [],
+  templateUrl: './wrapper.component.html',
+  styleUrl: './wrapper.component.css'
+})
+export class WrapperComponent implements OnInit {
+
+  counter = signal(0);
+
+  ngOnInit(): void {
+    console.log(`counter: ${this.counter()}`);
+    
+  }
+
+  increment(){
+
+    const value = this.counter() + 1;
+    this.counter.set(value);
+    
+  }
+
+}
