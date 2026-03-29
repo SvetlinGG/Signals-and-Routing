583d6f9a80d8ad4b721220f61c40ec321b154bb0
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-29 15:35:26 +0200

    make get requests

--- signals/src/app/users/user-service.service.ts
+++ signals/src/app/users/user-service.service.ts
@@ -1,14 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../types/users';
+
+
 
 @Injectable({
   providedIn: 'root'
+
+  
 })
 export class UserServiceService {
   apiUrl = 'https://jsonplaceholder.typicode.com/users'
   constructor(private http: HttpClient) { }
 
   getUsers(){
-    return this.http.get(this.apiUrl)
+    return this.http.get<User[]>(this.apiUrl)
+  }
+
+  getSingleUser(userId: number){
+    return this.http.get<User>(`${this.apiUrl}/${userId}`)
   }
 }
