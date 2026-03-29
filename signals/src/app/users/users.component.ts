7e0c6f94a2e2992f07f484df1eb13d5bf4825f4b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-29 16:21:21 +0200

    get users

--- signals/src/app/users/users.component.ts
+++ signals/src/app/users/users.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserServiceService } from './user-service.service';
+import { User } from '../types/users';
 
 @Component({
   selector: 'app-users',
@@ -7,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  users: User[] =[];
+  constructor(private userService: UserServiceService ){}
 
+ ngOnInit(): void {
+   this.fetchUsers();
+ }
+
+ fetchUsers(): void {
+  this.userService.getUsers().subscribe( users => {
+    console.log(users);
+    
+    this.users = users;
+  })
+ }
 }
