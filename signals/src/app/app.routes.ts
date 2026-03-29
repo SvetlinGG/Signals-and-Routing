9e8b172a8bf4f30dd3b3a474c15c899b1766cb88
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-29 14:57:00 +0200

    create user service

--- signals/src/app/app.routes.ts
+++ signals/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [
-    {path: "/", component: HomeComponent},
+    {path: "", component: HomeComponent},
     {path: "users", component: UsersComponent},
 ];
--- signals/src/app/navigation-bar/navigation-bar.component.ts
+++ signals/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { HomeComponent } from '../home/home.component';
+import { UsersComponent } from '../users/users.component';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
 })
