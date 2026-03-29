d750488cc6b88e45eee958f7c3fd9a1865bd2a58
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-29 16:30:48 +0200

    setup more style

--- signals/src/app/users/users.component.ts
+++ signals/src/app/users/users.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from './user-service.service';
 import { User } from '../types/users';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
