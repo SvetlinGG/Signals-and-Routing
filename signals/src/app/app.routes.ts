1cd4add0426ab4831c0586675360fdcd2de891b1
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-29 16:50:23 +0200

    create error page

--- signals/src/app/app.routes.ts
+++ signals/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './users/user-details/user-details.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const routes: Routes = [
-    {path: "", component: HomeComponent},
+    {path: "", redirectTo: "/home", pathMatch: "full"},
+    {path: "home", component: HomeComponent},
     {path: "users", component: UsersComponent},
+    {path: "users/details/:id", component: UserDetailsComponent},
+    {path: "**", component: ErrorPageComponent}
 ];
