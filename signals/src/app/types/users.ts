1cb0d2877015d825856730c2bb3847f19b018c61
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-29 15:18:31 +0200

    make types of Address, Users and other

---
+++ signals/src/app/types/users.ts
@@ -0,0 +1,26 @@
+import { Company } from "./company";
+
+export interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: string;
+    website: string;
+    company: Company;
+}
+
+export interface Geo {
+        lat: string;
+        lng: string;
+}
+
+export interface Address {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: Geo;
+    
+}
--- signals/src/app/users/user-service.service.ts
+++ signals/src/app/users/user-service.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
+  apiUrl = 'https://jsonplaceholder.typicode.com/users'
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getUsers(){
+    return this.http.get(this.apiUrl)
+  }
 }
