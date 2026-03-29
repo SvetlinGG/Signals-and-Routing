83a409892897b44e5bbd1b8c62033c0668e409ea
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-29 14:27:00 +0200

    styling components

--- signals/src/app/app.component.ts
+++ signals/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 //import { WrapperComponent } from './wrapper/wrapper.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
